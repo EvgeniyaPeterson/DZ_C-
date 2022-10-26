da04a204bff0cebca5195b6695a29e68adc45ff2
«Evgeniya» <e.madonova@gmail.com>
2022-10-26 16:35:50 +0300

    задача1.начало

---
+++ dz_001/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Задача 1.");
+Console.WriteLine("Введите число а");
+
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число b");
+int b = int.Parse(Console.ReadLine()!);
+
