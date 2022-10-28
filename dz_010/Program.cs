1fca9ab390e42ae5961cfce077cd0512e5e08c73
«Evgeniya» <e.madonova@gmail.com>
2022-10-28 20:21:42 +0300

    задача 10

---
+++ dz_010/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//Задача 10: Напишите программу, которая принимает на вход 
+//трёхзначное число и на выходе показывает вторую цифру 
+//этого числа. Через строку решать нельзя.
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.WriteLine("Задача 10");
+Console.WriteLine("Введите 3-х значное число:");
+
+int a = int.Parse(Console.ReadLine()!);
+
+if(a<100 || a>999)
+{
+    Console.WriteLine("Ошибка. Введите 3-х значное число:");
+}
+else
+{
+    a=a-(a-(a%100));
+    a=a/10;
+    Console.WriteLine(a);
+}
