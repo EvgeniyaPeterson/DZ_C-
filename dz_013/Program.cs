25ca7ea8122976ee1c0ceb492cace47b1fe46bcc
«Evgeniya» <e.madonova@gmail.com>
2022-11-11 11:52:02 +0300

    Домашнее задание2

---
+++ dz_013/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+//Задача 13: Напишите программу, которая выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет.Через 
+//строку решать нельзя.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+//Console.WriteLine("Задача 13.");
+
+int GetThirdDigit(int Num)
+{
+    while (Num > 1000)
+        Num = Num/10; 
+    if (Num >=100 && Num < 1000)
+        return = Num % 10;
+    else
+        return -1;
+}
+Console.WriteLine("Введите число ");
+int Num = int.Parse(Console.ReadLine()!);
+int ThirdDigit=GetThirdDigit(Num);
+if (ThirdDigit==-1)
+    Console.WriteLine("Третьего числа нет");
+else
+    Console.WriteLine("Третье число: " + ThirdDigit);
+
+
+
+
+
+
---
+++ dz_015/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход
+ //цифру, обозначающую день недели, и проверяет, является
+ // ли этот день выходным.
+
+int ReadDayNumber()
+{
+Console.WriteLine("Введите номер дня недели");
+int day = int.Parse(Console.ReadLine()!);
+if (day < 8 && day > 0)
+    return day;
+else
+    return -1;
+}
+
+int day = ReadDayNumber();
+if(day==-1)
+    Console.WriteLine("Такого дня в неделе нет!");
+else
+{
+    if(day<6)
+        Console.WriteLine("День не выходной!");
+    else
+        Console.WriteLine("Выходной!!!");    
+}
